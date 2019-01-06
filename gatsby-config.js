8d210a854bfd65b7faf210fa5111d1231377935e
Lennart <lekoarts@gmail.com>
2019-01-06 19:31:52 +0100

    Add backdrop: true to plugin's options

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,8 @@ module.exports = {
               movies: ['accountFavoriteMovies']
             }
           }
-        }
+        },
+        backdrop: true,
       }
     },
     `gatsby-plugin-accessibilityjs`,
