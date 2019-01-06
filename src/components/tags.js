1965a86687b53122109db75a1bcf13a99ec4e279
Sara Vieira <hey@iamsaravieira.com>
2019-01-06 21:53:25 +0100

    add share

--- src/components/tags.js
+++ src/components/tags.js
@@ -5,31 +5,43 @@ const Tags = ({ title, description }) => (
   <Helmet title={title}>
     <html lang="en" />
     <meta charset="utf-8" />
-    <meta name="description" content={description} />
+    <meta
+      name="description"
+      content="All the terrible horror movies you can handle"
+    />
     <meta
       name="image"
-      content="https://transitionlink.tylerbarnes.ca/static/e8d87a8b10d9cf54dc5c642854738957/419fb/transition-link-logo-2.png"
+      content="https://rawcdn.githack.com/SaraVieira/shitty-horror-movie.club/31b1016d733256b5071db6588a077b0f3f79b92a/src/images/favicon.png"
     />
     <meta itemProp="name" content="App" />
-    <meta itemProp="description" content={description} />
+    <meta
+      itemProp="description"
+      content="All the terrible horror movies you can handle"
+    />
     <meta
       itemProp="image"
-      content="https://transitionlink.tylerbarnes.ca/static/e8d87a8b10d9cf54dc5c642854738957/419fb/transition-link-logo-2.png"
+      content="https://rawcdn.githack.com/SaraVieira/shitty-horror-movie.club/31b1016d733256b5071db6588a077b0f3f79b92a/src/images/favicon.png"
     />
     <meta name="twitter:card" content="summary" />
     <meta name="twitter:title" content="App" />
-    <meta name="twitter:description" content={description} />
+    <meta
+      name="twitter:description"
+      content="All the terrible horror movies you can handle"
+    />
     <meta name="twitter:site" content="@NikkitaFTW" />
     <meta name="twitter:creator" content="@NikkitaFTW" />
     <meta
       name="twitter:image:src"
-      content="https://transitionlink.tylerbarnes.ca/static/e8d87a8b10d9cf54dc5c642854738957/419fb/transition-link-logo-2.png"
+      content="https://rawcdn.githack.com/SaraVieira/shitty-horror-movie.club/31b1016d733256b5071db6588a077b0f3f79b92a/src/images/favicon.png"
     />
     <meta name="og:title" content="App" />
-    <meta name="og:description" content={description} />
+    <meta
+      name="og:description"
+      content="All the terrible horror movies you can handle"
+    />
     <meta
       name="og:image"
-      content="https://transitionlink.tylerbarnes.ca/static/e8d87a8b10d9cf54dc5c642854738957/419fb/transition-link-logo-2.png"
+      content="https://rawcdn.githack.com/SaraVieira/shitty-horror-movie.club/31b1016d733256b5071db6588a077b0f3f79b92a/src/images/favicon.png"
     />
     <meta name="og:url" content="https://example.com" />
     <meta name="og:site_name" content="App" />
